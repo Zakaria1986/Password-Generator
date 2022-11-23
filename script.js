4720dd0698c4d737508933f0c220b0e5969bb606
Zak khan <zakaria2012@gmail.com>
2022-11-23 07:35:43 +0000

    put code into functions and made the code have the desired out put

--- script.js
+++ script.js
@@ -88,116 +88,100 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// var arr = [...upperCasedCharacters, ...lowerCasedCharacters, ...specialCharacters, ...numericCharacters];
 
 
-// Generating raondom numbers test - i'm gonna go with code below as this as this seem more fun
 
-// var arrOriginal = [upperCasedCharacters, lowerCasedCharacters, specialCharacters, numericCharacters];
-// console.log(arrOriginal);
-var arr = [];
-// console.log(arr);
-
-var userInput;
-userInput = prompt("Enter a number for password between 10 to 64: ");
-
-console.log("Initial value: ", userInput);
+// console.log(emptArr.includes(userPassCriteria[0].upperCasedCharacters));
 
 
-if (userInput < 10 || userInput > 64) {
-  for (; true;) {
-    alert("Your input: " + userInput + " Please enter number between 10 to 64");
-    userInput = prompt("Enter a number for password should be at least 10 or more but not more than 64: ");
-    console.log("Initial value: ", userInput);
-    if (userInput >= 10 && userInput <= 64) {
-      break;
-    }
-    else { continue; }
-  }
-}
-
-// Write confirms dailog box here and then based on user choice add or remove the characters from the list
-
-var userPassCriteria = [
-  {
-    userChoice: confirm("Do you want special charectors?"),
-    characters: specialCharacters
-  },
-  {
-    userChoice: confirm("Do you want special lower cased characters?"),
-    characters: lowerCasedCharacters
-  },
-  {
-    userChoice: confirm("Do you want Numbers added to your password?"),
-    characters: numericCharacters
-  },
-  {
-    userChoice: confirm("do you want add Uppder case to your password?"),
-    characters: upperCasedCharacters
-  }
-]
-
-userPassCriteria.forEach(items => {
-  console.log(items.userChoice);
-  if (items.userChoice === true) {
-    arr.push(items.characters);
-  }
-});
-
-
-console.log(arr);
-
-var emptArr = [];
-
-for (var i = emptArr.length; i < userInput; i++) {
-
-  var indexRandom = Math.floor(Math.random(arr.length) * arr.length);
-  console.log('Random computer choice of index: ', indexRandom);
-
-  var accessRandonArrays = Math.floor(Math.random(arr[indexRandom].length) * arr[indexRandom].length);
-  console.log(accessRandonArrays);
-  var char = arr[indexRandom][accessRandonArrays];
-
-  emptArr.push(char);
-
-}
-console.log(emptArr);
 
 
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  // Ask user the options of enter the length of password and the inlusion of the character
-  // if the length is greater than 8 and less than 16 and less. 
-  //var passLength = prompt('Enter password between 10 to 64 characters: ');
+  // var arr = [...upperCasedCharacters, ...lowerCasedCharacters, ...specialCharacters, ...numericCharacters];
+  // Generating raondom numbers test - i'm gonna go with code below as this as this seem more fun
+  // var arrOriginal = [upperCasedCharacters, lowerCasedCharacters, specialCharacters, numericCharacters];
+  // console.log(arrOriginal);
+  var arr = [];
+  // console.log(arr);
 
-  // if (passLength > 0 && passLength < 10 && passLength > 64) {
-  //   prompt("Your password should be at least 10 characters but no more than 64");
-  // }
-  // if not true prompt to enter numbers between the lesson 
-  // if it is true then enter ask if they wanna innclude numbers,
-  // if it is true then enter ask if they wanna innclude symbol,
-  // if it is true then enter as if they wanna innclude Upper case letters,
-  // if it is true then enter as if they wanna innclude lower case letters
+  var userInput;
+  userInput = prompt("Enter a number for password between 10 to 64: ");
+  console.log("Initial value: ", userInput);
 
-  // if all true or base on some option get random letters and symbols 
+  if (userInput < 10 || userInput > 64) {
+    for (; true;) {
+      alert("Your input: " + userInput + " Please enter number between 10 to 64");
+      userInput = prompt("Enter a number for password should be at least 10 or more but not more than 64: ");
+      console.log("Initial value: ", userInput);
+      if (userInput >= 10 && userInput <= 64) {
+        break;
+      }
+      else { continue; }
+    }
+  }
 
+
+  // Write confirms dailog box here and then based on user choice add or remove the characters from the list
+
+  var userPassCriteria = [
+    {
+      userChoice: confirm("Do you want special charectors?"),
+      characters: specialCharacters
+    },
+    {
+      userChoice: confirm("Do you want special lower cased characters?"),
+      characters: lowerCasedCharacters
+    },
+    {
+      userChoice: confirm("Do you want Numbers added to your password?"),
+      characters: numericCharacters
+    },
+    {
+      userChoice: confirm("do you want add Uppder case to your password?"),
+      characters: upperCasedCharacters
+    }
+  ]
+
+  userPassCriteria.forEach(items => {
+    console.log(items.userChoice);
+    if (items.userChoice === true) {
+      arr.push(items.characters);
+    }
+  });
+
+  console.log(arr);
+  var output = [arr, userInput];
+  return output;
 }
-var getPasswordOptions = getPasswordOptions();
-console.log(getPasswordOptions);
 
 
 // Function for getting a random element from an array
 function getRandom(arr) {
 
+  var emptArr = [];
 
+  for (var i = emptArr.length; i < arr[1]; i++) {
+
+    var indexRandom = Math.floor(Math.random(arr[0].length) * arr[0].length);
+    console.log('Random computer choice of index: ', indexRandom);
+    var accessRandonArrays = Math.floor(Math.random(arr[0][indexRandom].length) * arr[0][indexRandom].length);
+    console.log(accessRandonArrays);
+    var char = arr[0][indexRandom][accessRandonArrays];
+    emptArr.push(char);
+  }
+
+  console.log(emptArr);
+
+  return emptArr;
 
 }
-getRandom();
 
 // Function to generate password with user input
 function generatePassword() {
-
+  var getPasswordOptions_output = getPasswordOptions()
+  getRandom(getPasswordOptions_output);
 }
 
 // Get references to the #generate element
